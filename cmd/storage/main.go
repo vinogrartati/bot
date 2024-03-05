42260f64ffd03849b3115e8f02fe93bdc9a49911
Vinogradova Tatyana <tanyavinogradova1@gmail.com>
2024-03-05 20:15:49 +1000

    Добавление логики

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -1,12 +1,23 @@
-package storage
+package main
 
 import (
 	"fmt"
+	"log"
 
 	"storage/internal/storage"
 )
 
 func main() {
 	st := storage.NewStorage()
-	fmt.Println("Hello world", st)
+
+	f, err := st.Upload("test.txt", []byte("Hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	restoredFile, err := st.GetById(f.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Hello world", restoredFile)
 }
